95fdbab1b4d584446a4f7a745fa4a042737e0f08
salman10118389 <salman.10118389@mahasiswa.unikom.ac.id>
2021-11-18 09:56:09 +0700

    Commit Pertama

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -6,7 +6,8 @@ class Admin extends CI_Controller {
 
 
 	public function __construct(){
-	    parent::__construct();
+	    parent::__construct();    
+        $this->load->helper(array('form', 'url'));
 		$this->session->set_userdata('website', "Admin");
 	    if(!$this->session->userdata('id')){
 			redirect("login/index");
@@ -104,6 +105,44 @@ class Admin extends CI_Controller {
 		print_r($nama.'.jpg');
 	}
 
+	function upload_image(){
+	    if(isset($_FILES["image"]["name"])){
+			$jenis = $this->input->post('jenis');
+			$namaGambar = strtolower(str_replace(" ", "-", $this->input->post('namaGambar')."-".rand(1,10000)));
+	    	$this->load->helper('file');
+			$image_path = realpath(APPPATH . '../gambar/'.$jenis."/");
+	        $config['upload_path'] = $image_path;
+	        $config['allowed_types'] = 'jpg|jpeg|png|gif';
+            $config['file_name']     = $namaGambar;
+	        $this->upload->initialize($config);
+	        if(!$this->upload->do_upload('image')){
+	            $this->upload->display_errors();
+	            return FALSE;
+	        }else{
+	            $data = $this->upload->data();
+	            //Compress Image
+	            $config['image_library']='gd2';
+	            $config['source_image']= $image_path.$data['file_name'];
+	            $config['create_thumb']= FALSE;
+	            $config['maintain_ratio']= TRUE;
+	            $config['quality']= '60%';
+	            $config['width']= 800;
+	            $config['height']= 800;
+	            $config['new_image']= $image_path.$data['file_name'];
+	            $this->load->library('image_lib', $config);
+	            $this->image_lib->resize();
+	            echo base_url(). '/gambar/'.$jenis."/".$namaGambar.".jpg";
+	        }
+	    }
+	}
+
+	function delete_image(){
+	    $src = $this->input->post('src');
+	    $file_name = str_replace(base_url(), '', $src);
+	    if(unlink($file_name)){
+	        echo 'File Delete Successfully';
+	    }
+	}
 
 	public function tambah_data()  
 	{
@@ -112,18 +151,144 @@ class Admin extends CI_Controller {
 		$data_insert = [];
 		if ($jenis ==  "admin"){
 	        $tabel = "admin_".$jenis;
-	        $data_insert = array(
-	            'nama' => $this->input->post('nama'),
-	            'username' => $this->input->post('username'),
-	            'photo' => $this->input->post('nama_photo'),
-	            'nomor_telepone' => $this->input->post('handphone'),
-	            'role' => $this->input->post('role'),
-	            'catatan' => $this->input->post('catatan')
+
+	        $url = "data-detail-".strtolower(str_replace(" ", "-", $this->input->post('nama')))."-".str_replace(" ", "-", strtolower($this->input->post('jabatan')))."-warung-freelancer";
+			
+			$this->load->helper('file');
+			$image_path = realpath(APPPATH . '../gambar/'.$jenis."/");
+			$config['upload_path']          = $image_path;
+            $config['allowed_types']        = 'gif|jpg|png';
+            $config['file_name']            = $url;
+			$config['overwrite']            = true;
+			// $config['max_size']             = 1024; // 1MB
+			// $config['max_width']            = 1080;
+			// $config['max_height']           = 1080;
+            $this->load->library('upload');
+            $this->upload->initialize($config);
+			$this->upload->do_upload('photo');
+
+			$optional = [];
+            $optional['select'] = "";
+            $where["field"][0] = "nama";
+            $where["velue"][0] = $this->input->post('jabatan'); 
+            $optional['where'] = $where;
+            $optional['search'] = "";
+            $optional['distinct'] = "";
+            $optional['join'] = "";
+            $optional['order_by'] = "";
+            $optional['limit'] = "";
+            $optional['offset'] = "";
+            $data = $this->modeladmin->ambil_data("web_kategori",$optional); 
+
+			$data_insert = array(
+	            'nama' 				=> $this->input->post('nama'),
+	            'username' 			=> $this->input->post('username'),
+	            'photo' 			=> $url.".jpg",
+	            'jabatan' 			=> $this->input->post('jabatan'),
+	            'aktif' 			=> $this->input->post('aktif'),
+	            'catatan'		 	=> $this->input->post('catatan'),
+	            'link'		 		=> $url,
+	            'slider'		 	=> $data[0]["gambar"],
+	            'slider_potrait'	=> $data[0]["gambar_potrait"],
+	            'password'			=> "test1234"
 	        );
+
+
 	        $result = $this->modeladmin->tambah_data($tabel, $data_insert, $jenis);
 	        redirect('admin/data-'.$jenis);
-		}
-		elseif ($jenis == "konten" && $kategori == "slider"){
+		} else if ($jenis == "jasa" ){
+			$tabel = "web_".$jenis;
+
+	        $urlSlider = strtolower(str_replace(" ", "-", $this->input->post('judul')))."-slider";
+			$this->load->helper('file');
+			$image_path = realpath(APPPATH . '../gambar/'.$jenis."/");
+			$config['upload_path']          = $image_path;
+            $config['allowed_types']        = 'gif|jpg|png';
+            $config['file_name']            = $urlSlider;
+			$config['overwrite']            = true;
+			// $config['max_size']             = 1024; // 1MB
+			// $config['max_width']            = 1080;
+			// $config['max_height']           = 1080;
+            $this->load->library('upload');
+            $this->upload->initialize($config);
+			$this->upload->do_upload('slider');
+
+			$urlSliderPotrait = strtolower(str_replace(" ", "-", $this->input->post('judul')))."-slider-potrait";
+			$this->load->helper('file');
+            $config['file_name'] = $urlSliderPotrait;
+            $this->upload->initialize($config);
+			$this->upload->do_upload('slider-potrait');
+
+			$urlGambar = strtolower(str_replace(" ", "-", $this->input->post('judul')));
+			$this->load->helper('file');
+            $config['file_name'] = $urlGambar;
+            $this->upload->initialize($config);
+			$this->upload->do_upload('gambar');
+
+			$data_insert = array(
+	            'judul' 				=> $this->input->post('judul'),
+	            'deskripsi_judul' 		=> $this->input->post('deskripsi'),
+	            'gambar' 				=> $urlGambar.".jpg",
+	            'gambar_header_potrait' => $urlSliderPotrait.".jpg",
+	            'gambar_header' 		=> $urlSlider.".jpg",
+	            'kategori_id' 			=> $this->input->post('kategori_id'),
+	            'favorit' 				=> $this->input->post('favorit'),
+	            'isi_artikel'		 	=> $this->input->post('artikel'),
+	            'link'		 			=> $urlGambar
+	        );
+
+	        $result = $this->modeladmin->tambah_data($tabel, $data_insert, $jenis);
+
+	        redirect('admin/konten-utama/konten-'.$jenis);
+
+
+		}else if ($jenis == "artikel" ){
+			$tabel = "web_".$jenis;
+
+	        $urlSlider = "artikel-".strtolower(str_replace(" ", "-", $this->input->post('judul')))."-slider";
+			$this->load->helper('file');
+			$image_path = realpath(APPPATH . '../gambar/'.$jenis."/");
+			$config['upload_path']          = $image_path;
+            $config['allowed_types']        = 'gif|jpg|png';
+            $config['file_name']            = $urlSlider;
+			$config['overwrite']            = true;
+			// $config['max_size']             = 1024; // 1MB
+			// $config['max_width']            = 1080;
+			// $config['max_height']           = 1080;
+            $this->load->library('upload');
+            $this->upload->initialize($config);
+			$this->upload->do_upload('slider');
+
+			$urlSliderPotrait = "artikel-".strtolower(str_replace(" ", "-", $this->input->post('judul')))."-slider-potrait";
+			$this->load->helper('file');
+            $config['file_name'] = $urlSliderPotrait;
+            $this->upload->initialize($config);
+			$this->upload->do_upload('slider-potrait');
+
+			$urlGambar = "artikel-".strtolower(str_replace(" ", "-", $this->input->post('judul')));
+			$this->load->helper('file');
+            $config['file_name'] = $urlGambar;
+            $this->upload->initialize($config);
+			$this->upload->do_upload('gambar');
+
+			$data_insert = array(
+	            'judul' 				=> $this->input->post('judul'),
+	            'deskripsi_judul' 		=> $this->input->post('deskripsi'),
+	            'gambar' 				=> $urlGambar.".jpg",
+	            'gambar_header_potrait' => $urlSliderPotrait.".jpg",
+	            'gambar_header' 		=> $urlSlider.".jpg",
+	            'kategori_id' 			=> $this->input->post('kategori_id'),
+	            'favorit' 				=> $this->input->post('favorit'),
+	            'isi_artikel'		 	=> $this->input->post('artikel'),
+	            'link'		 			=> $urlGambar
+	        );
+
+	        $result = $this->modeladmin->tambah_data($tabel, $data_insert, $jenis);
+
+	        redirect('admin/konten-utama/konten-'.$jenis);
+
+
+		}else if ($jenis == "konten" && $kategori == "slider"){
 			
 			$tabel = "web_".$kategori;
 			$data_insert = array(
@@ -135,32 +300,47 @@ class Admin extends CI_Controller {
 	        $result = $this->modeladmin->tambah_data($tabel, $data_insert, $jenis, $kategori);
 	        redirect('admin/data-'.$jenis."/".'data-'.$kategori.'s');
 		}
-       
-    	
     }
 
 
 	public function ubah_data()  
 	{
 		$jenis = $this->input->post('jenis');
-		$tabel = "admin_".$jenis;
-		$data_insert = [];
-		$where = "id";
-		$id = $this->input->post('id');
+		if ($jenis == "Admin"){
+			$tabel = "admin_".$jenis;
+			$data_insert = [];
+			$where = "id";
+			$id = $this->input->post('id');
 
-		if ($jenis ==  "admin"){
-	        $data_insert = array(
-	            'nama' => $this->input->post('nama'),
-	            'username' => $this->input->post('username'),
-	            'photo' => $this->input->post('nama_photo'),
-	            'nomor_telepone' => $this->input->post('handphone'),
-	            'role' => $this->input->post('role'),
-	            'catatan' => $this->input->post('catatan')
-	        );
+			if (!$_FILES['photo']['size'] == 0 && !$_FILES['photo']['error'] == 0){
+			    $this->load->helper('file');
+				$image_path = realpath(APPPATH . '../gambar/'.$jenis."/");
+				$config['upload_path']          = $image_path;
+	            $config['allowed_types']        = 'gif|jpg|png';
+	            $config['file_name']            = $this->input->post('link');
+				$config['overwrite']            = true;
+				// $config['max_size']             = 1024; // 1MB
+				// $config['max_width']            = 1080;
+				// $config['max_height']           = 1080;
+	            $this->load->library('upload');
+	            $this->upload->initialize($config);
+				$this->upload->do_upload('photo');
+			} 
+
+			if ($jenis ==  "admin"){
+		        $data_insert = array(
+		            'nama' => $this->input->post('nama'),
+		            'username' => $this->input->post('username'),
+		            'photo' => $this->input->post('nama_photo'),
+		            'nomor_telepone' => $this->input->post('handphone'),
+		            'role' => $this->input->post('role'),
+		            'catatan' => $this->input->post('catatan')
+		        );
+			}
+
+	        $result = $this->modeladmin->ubah_data($tabel, $data_insert, $jenis, $id, $where);
+	    	redirect('admin/data-'.$jenis);			
 		}
-
-        $result = $this->modeladmin->ubah_data($tabel, $data_insert, $jenis, $id, $where);
-    	redirect('admin/data-'.$jenis);
     }
 
     public function hapus_data(){
@@ -170,14 +350,18 @@ class Admin extends CI_Controller {
     		$data["id"] = $this->input->post('id');
 	    	$data["nama"] = $this->input->post('nama');
 	    	$tabel = "admin_".$this->input->post('jenis');
-	    	if($this->input->post('jenis') == "operasional"){
-    			$tabel = "biaya_operasional";
-    		}
-    	}
-    	elseif($kategori == "slider" ){
+    	}else if($jenis == "slider" ){
     		$data["id"] = $this->input->post('id');
 	    	$data["nama"] = $this->input->post('nama');
-	    	$tabel = "web_".$this->input->post('kategori');
+	    	$tabel = "web_".$this->input->post('jenis');
+    	}else if($jenis == "artikel" ){
+    		$data["id"] = $this->input->post('id');
+	    	$data["nama"] = $this->input->post('nama');
+	    	$tabel = "web_".$this->input->post('jenis');
+    	}else if($jenis == "jasa" ){
+    		$data["id"] = $this->input->post('id');
+	    	$data["nama"] = $this->input->post('nama');
+	    	$tabel = "web_".$this->input->post('jenis');
     	}
         $result = $this->modeladmin->hapus_data($tabel, $data, $kategori);
         return $result;
@@ -222,31 +406,5 @@ class Admin extends CI_Controller {
 		$this->session->set_userdata($type_menu, "in");
         redirect('admin/developer/menu');
 	}
-
-	
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 }
